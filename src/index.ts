b2e85f0dfbd79599626e631778bbe24a4b458882
IT-WIBRC <wibrc.se.jc@gmail.com>
2024-02-19 19:01:26 +0100

    refactor: add the script to init the database table

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 require('dotenv').config();
 
 import { app } from "./infra/http/app";
+import { connection } from "./infra/typeorm/models";
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`[App]: Server listening on ${PORT}`)
-});
+(async function () {
+    try {
+      // await connection.initialize();
+  
+      const PORT = process.env.PORT || 5000;
+      app.listen(PORT, () => {
+        console.log(`[App]: Server listening on ${PORT}`);
+      });
+    } catch (error: unknown) {
+      console.log(
+        "Server has not started due to : ",
+        (error as { message: string }).message
+      );
+    }
+  })();
